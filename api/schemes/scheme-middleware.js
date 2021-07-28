2f148a537462ae09db211da035f7851ba800fbae
Kishkuman13 <73566523+Kishkuman13@users.noreply.github.com>
2021-07-28 00:13:02 -0600

    MVP wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Schemes = require('./scheme-model');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,7 +9,18 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
+  const id = req.params.scheme_id;
+  Schemes.findById(id)
+    .then(scheme => {
+      if (scheme) {
+        req.scheme = scheme;
 
+        next();
+      } else {
+        res.status(404).json({ message: `scheme with scheme_id ${id} not found` })
+      }
+    })
+    .catch(err => next(err))
 }
 
 /*
@@ -19,7 +32,13 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  const schemeData = req.body;
 
+  if ( !schemeData || !schemeData.scheme_name ) {
+    res.status(400).json({ message: 'invalid scheme_name'})
+  } else {
+    next();
+  }
 }
 
 /*
@@ -32,7 +51,13 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const schemeData = req.body;
 
+  if ( !schemeData || !schemeData.instructions || !schemeData.step_number === NaN || !schemeData.step_number < 1 ) {
+    res.status(400).json({ message: 'invalid step'})
+  } else {
+    next();
+  }
 }
 
 module.exports = {
